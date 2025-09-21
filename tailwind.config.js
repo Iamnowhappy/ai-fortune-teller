c5aa2bfffd8f47baa0c2e31fd80794a7cfc7562c
sgc <hsgchng@naver.com>
2025-09-21 14:57:03 +0900

    build: Finalize and unify project to standard Vite configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,12 @@ export default {
     "./**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'sans': ['"Noto Sans KR"', 'sans-serif'],
+        'display': ['"Playfair Display"', 'serif'],
+      }
+    },
   },
   plugins: [],
 }
