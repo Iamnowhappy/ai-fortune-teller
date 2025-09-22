302c611b5fa51da4fd1bd66f38472288b5bf7666
sgc <hsgchng@naver.com>
2025-09-23 05:43:15 +0900

    feat: 구글 한글 이상번역 수정

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,18 +1,14 @@
 import React from 'react';
 import { AdSenseUnit } from './AdSenseUnit';
 
-interface FooterProps {
-  currentPage: string;
-}
-
-export const Footer: React.FC<FooterProps> = ({ currentPage }) => {
+export const Footer: React.FC = () => {
   return (
     <footer className="text-center py-6 border-t border-slate-700/50 mt-12">
       {/* 
         !!! 중요: 아래 "YOUR_AD_SLOT_ID"를 
         실제 Google AdSense 광고 슬롯 ID로 교체해야 광고가 표시됩니다. 
       */}
-      <AdSenseUnit key={currentPage} adSlot="YOUR_AD_SLOT_ID" />
+      <AdSenseUnit adSlot="YOUR_AD_SLOT_ID" />
       
       <p className="text-sm text-slate-500">
         본 서비스는 재미를 위해 제공되며, 과학적 근거가 없습니다. 결과는 참고용으로만 활용해주세요.
