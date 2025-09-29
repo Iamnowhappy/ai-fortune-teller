af2aef9039d3af9e93976f588310ab12e93d44a6
sgc <hsgchng@naver.com>
2025-09-29 19:34:28 +0900

    fix : 이름 작명 추가오류 수정

--- components/icons.tsx
+++ components/icons.tsx
@@ -166,6 +166,14 @@ export const DreamIcon: React.FC<IconProps> = ({ className }) => (
   </svg>
 );
 
+export const NameGeneratorIcon: React.FC<IconProps> = ({ className }) => (
+    <svg className={className} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+        <path d="M12 22h6"/>
+        <path d="M21 16V8a2 2 0 0 0-1-1.73l-7-4a2 2 0 0 0-2 0l-7 4A2 2 0 0 0 3 8v8a2 2 0 0 0 1 1.73l7 4a2 2 0 0 0 2 0l2-1.14"/>
+        <path d="M16.5 9.4a2 2 0 0 1 0 2.8"/>
+        <path d="M12 6L3.5 11"/>
+    </svg>
+);
 
 export const BoxIcon: React.FC<IconProps> = ({ className }) => (
   <svg className={className} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
